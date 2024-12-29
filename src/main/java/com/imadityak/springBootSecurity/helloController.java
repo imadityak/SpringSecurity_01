74f24777878116ad048581f19f100585c03d8d4e
imadityak <adityacoding.one@gmail.com>
2024-12-29 15:31:04 +0530

    revert helloController

--- src/main/java/com/imadityak/springBootSecurity/helloController.java
+++ src/main/java/com/imadityak/springBootSecurity/helloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
 
     @GetMapping("/")
-    public String greet(){
-        return "Session ID: ";
+    public String greet(HttpServletRequest request){
+        return "Session ID: " + request.getSession().getId();
     }
 }
