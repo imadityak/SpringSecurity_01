f6db04a0c1211d376df4fc5dcc21268a6f2c9b1b
imadityak <adityacoding.one@gmail.com>
2025-01-09 14:17:29 +0530

    crsf token

--- src/main/java/com/imadityak/springBootSecurity/StudentController.java
+++ src/main/java/com/imadityak/springBootSecurity/StudentController.java
@@ -1,6 +1,11 @@
 package com.imadityak.springBootSecurity;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,5 +23,16 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
     }
 }
